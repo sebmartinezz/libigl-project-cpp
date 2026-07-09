692f02179fb1b0d7d2b1d9190708181f0691826f
esortizs-create <esortizs@unal.edu.co>
2026-07-08 20:20:31 -0500

    Update deformation.h

--- include/geometry/deformation.h
+++ include/geometry/deformation.h
@@ -3,3 +3,6 @@
 #include <vector>
 
 void twist (Mesh&mesh, float strength, float y0, float max_angle);
+void bend(Mesh& mesh, float radius);
+void ripple(Mesh& mesh, float amplitude, float frequency);
+void bulge(Mesh& mesh, float strength);
